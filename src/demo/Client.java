e3624277dcec4bca9f5ebed1eb46bc69f9138d13
unknown <liuyczj@126.com>
2019-08-29 11:06:58 +0800

    输入和输出练习

--- src/demo/Client.java
+++ src/demo/Client.java
@@ -3,8 +3,12 @@ package demo;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 第01步：创建scanner对象
+		
+		// 第02步：接收用户的输入
+		
+		// 第03步：输出欢迎信息，比如："你好，张三"
+        
 	}
 
 }
